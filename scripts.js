a005440b09c3b9f13f4892ebdec0c82d37a00d98
TheScooterMcGee <68203663+TheScooterMcGee@users.noreply.github.com>
2020-08-05 12:00:03 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -847,4 +847,158 @@ $(document).ready(function() {
             }
         }
     });
+    
+    function save() {
+        // Reset variables
+        let removed = [];
+        let favourite = [];
+        for (let i = 0; i < names.length; i++) {
+            if (names[i].removed === true) {
+                removed.push(names[i]);
+            }
+            if (names[i].favourite === true) {
+                favourite.push(names[i]);
+            }
+        }
+        Cookies.set("removed", removed, {expires: 365});
+        Cookies.set("favourite", favourite, {expires: 365});
+    }
+    function load() {
+        let removed = Cookies.get("removed");
+        let favourite = Cookies.get("favourite");
+        // Resets classes
+        $(".fighter-name").each(function() {
+            $(this).removeClass("removed favourite");
+        });
+        // Resets values
+        for(let i = 0; i < names.length; i++) {
+            names[i].removed = false;
+            names[i].favourite = false;
+        }
+        // Adds removed
+        for(let i = 0; i < removed.length; i++) {
+            removed[i].removed = true;
+        }
+        // Adds favourites
+        for(let i = 0; i < favourite.length; i++) {
+            favourite[i].removed = true;
+        }
+        // Adds removed classes
+        for (let i = 0; i < removed.length; i++) {
+            $(".fighter-name").each(function() {
+                if ($(this).data("fighter") === removed[i].name) {
+                    $(this).addClass("removed");
+                }
+            });
+        }
+        // Adds favourite classes
+        for (let i = 0; i < favourite.length; i++) {
+            $(".fighter-name").each(function() {
+                if ($(this).data("fighter") === favourite[i].name) {
+                    $(this).addClass("favourite");
+                }
+            });
+        }
+    }
+
+    $("#save_btn").click(function() {
+        save();
+    });
+    $("#load_btn").click(function() {
+        load(this);
+    });
+
+    $('#DKTIME').click(function(){
+        $("#random-img").attr("src", "../assets/fighters/donkey_kong.webp");
+        $("#random-logo").attr("src", "../assets/logos/donkey_kong.svg");
+        $("#random-name").text("Donkey Kong");
+        $("#random-colour").css("background", "linear-gradient(-45deg, #f9df64 20%, transparent 80%)");
+        for(let i = 0; i < names.length; i++) {
+            names[i].removed = true;
+        }
+        $(".fighter-name").each(function() {
+            let element = $(this).data("fighter");
+            if (element != "Donkey Kong") {
+                $(this).addClass("removed");
+            }
+        });
+        names[1].removed = false;
+
+        $("#container").fadeIn('slow');
+        //Makes Screen golden
+        $("#container").addClass("DKTIME-container");
+        $('#DKTIME').css("pointer-events", "none");
+        //DK RAP
+        $('#dk_rap').get(0).play();
+        setTimeout(function() {
+            $("#container").fadeOut('slow');
+            $('#DKTIME').css("pointer-events", "auto");
+        }, 34500);
+        // Makes Bananas rain
+        var container = document.getElementById('animate');
+        var emoji = ['🍌'];
+        var circles = [];
+        
+        for (var i = 0; i < 15; i++) {
+          addCircle(i * 150, [10 + 0, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+          addCircle(i * 150, [10 + 0, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+          addCircle(i * 150, [10 - 200, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+          addCircle(i * 150, [10 + 200, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+          addCircle(i * 150, [10 - 400, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+          addCircle(i * 150, [10 + 400, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+          addCircle(i * 150, [10 - 600, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+          addCircle(i * 150, [10 + 600, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+        }
+        
+        
+        
+        function addCircle(delay, range, color) {
+          setTimeout(function() {
+            var c = new Circle(range[0] + Math.random() * range[1], 80 + Math.random() * 4, color, {
+              x: -0.15 + Math.random() * 0.3,
+              y: 1 + Math.random() * 1
+            }, range);
+            circles.push(c);
+          }, delay);
+        }
+        
+        function Circle(x, y, c, v, range) {
+          var _this = this;
+          this.x = x;
+          this.y = y;
+          this.color = c;
+          this.v = v;
+          this.range = range;
+          this.element = document.createElement('span');
+          /*this.element.style.display = 'block';*/
+          this.element.style.opacity = 0;
+          this.element.style.position = 'absolute';
+          this.element.style.fontSize = '26px';
+          this.element.style.color = 'hsl('+(Math.random()*360|0)+',80%,50%)';
+          this.element.innerHTML = c;
+          container.appendChild(this.element);
+        
+          this.update = function() {
+            if (_this.y > 800) {
+              _this.y = 80 + Math.random() * 4;
+              _this.x = _this.range[0] + Math.random() * _this.range[1];
+            }
+            _this.y += _this.v.y;
+            _this.x += _this.v.x;
+            this.element.style.opacity = 1;
+            this.element.style.transform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
+            this.element.style.webkitTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
+            this.element.style.mozTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
+          };
+        }
+        
+        function animate() {
+          for (var i in circles) {
+            circles[i].update();
+          }
+          requestAnimationFrame(animate);
+        }
+        
+        animate();
+    });
 });
